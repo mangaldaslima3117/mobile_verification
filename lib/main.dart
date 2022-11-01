939db338a6734e92f9e1484ec3bc60b74cade211
mangaldaslima3117 <mangaldas.lima@gmail.com>
2022-11-01 16:55:53 +0530

    push notification in flutter web

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_signup/home.dart';
 import 'package:firebase_signup/landing_page.dart';
 import 'package:firebase_signup/page/item_list_page.dart';
@@ -16,11 +17,11 @@ Future<void> main() async {
   //await Firebase.initializeApp();
   await Firebase.initializeApp(
     options: FirebaseOptions(
-      apiKey: 'AIzaSyBGJ4bjcTuRuGkT04fdS5ZuPBtCHmfnHlE',
-      appId: '1:154285814531:android:9219aa8562cf9b41932f36',
-      messagingSenderId: '',
-      projectId: 'flutter-tutorial-cf212',
-      storageBucket: "flutter-tutorial-cf212.appspot.com",
+      apiKey: 'AIzaSyBREdiDdOJNdvJDIRYrLYraWNVlalSvlJs',
+      appId: '1:897956436289:web:a88b4ac73fb4baced4a9c0',
+      messagingSenderId: '897956436289',
+      projectId: 'flutter-tutorial-843d7',
+      storageBucket: "flutter-tutorial-843d7.appspot.com",
     ),
   );
   runApp(MyApp());
@@ -63,29 +64,62 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   @override
+  void initState() {
+    getTokent();
+    FirebaseMessaging.onMessage.listen(showFlutterNotification);
+    super.initState();
+  }
+
+  getTokent() async {
+    String deviceToken = await FirebaseMessaging.instance.getToken();
+    print('DEVICE TOKEN ' + deviceToken);
+  }
+
+  void showFlutterNotification(RemoteMessage message) {
+    RemoteNotification notification = message.notification;
+    AndroidNotification android = message.notification?.android;
+    print('NOTIFICATION TITLE ' + notification.title.toString());
+    showModalBottomSheet(
+      context: context,
+      builder: ((context) {
+        return Container(
+          child: Column(
+            children: [
+              Text(notification.title),
+              Text(
+                notification.body,
+              ),
+            ],
+          ),
+        );
+      }),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //body: Account(),
-      body: ItemListPage()
-      // body: StreamBuilder(
-      //   stream: FirebaseAuth.instance.authStateChanges(),
-      //   builder: (context, snapshot) {
-      //     print('PRINT AUTHSTATE DATA ');
-      //     print(snapshot);
-      //     print(snapshot.hasData);
-      //     if (ConnectionState.waiting == snapshot.connectionState) {
-      //       return Container(
-      //         child: CircularProgressIndicator.adaptive(),
-      //       );
-      //     }
+        //body: Account(),
+        body: ItemListPage()
+        // body: StreamBuilder(
+        //   stream: FirebaseAuth.instance.authStateChanges(),
+        //   builder: (context, snapshot) {
+        //     print('PRINT AUTHSTATE DATA ');
+        //     print(snapshot);
+        //     print(snapshot.hasData);
+        //     if (ConnectionState.waiting == snapshot.connectionState) {
+        //       return Container(
+        //         child: CircularProgressIndicator.adaptive(),
+        //       );
+        //     }
 
-      //     if (snapshot.hasData) {
-      //       return LandingPage();
-      //     } else {
-      //       return Signin();
-      //     }
-      //   },
-      // ),
-    );
+        //     if (snapshot.hasData) {
+        //       return LandingPage();
+        //     } else {
+        //       return Signin();
+        //     }
+        //   },
+        // ),
+        );
   }
 }
---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,21 @@
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBREdiDdOJNdvJDIRYrLYraWNVlalSvlJs",
+    authDomain: "flutter-tutorial-843d7.firebaseapp.com",
+    projectId: "flutter-tutorial-843d7",
+    storageBucket: "flutter-tutorial-843d7.appspot.com",
+    messagingSenderId: "897956436289",
+    appId: "1:897956436289:web:a88b4ac73fb4baced4a9c0",
+    measurementId: "G-8WJ0DL1N8V"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+// Optional:
+messaging.onBackgroundMessage((message) => {
+    console.log("onBackgroundMessage", message);
+});
